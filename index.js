00c9216dda55a284998b8bdf677dccb103d36f0b
valma <muceravn@gmail.com>
2022-06-27 12:49:39 +0300

    Complete js file

--- index.js
+++ index.js
@@ -34,3 +34,77 @@ else{
 }
 }
 
+const searchMeal = () =>{
+    const mealInput = document.getElementById('meal-input').value;
+
+    // Checking if the user searched for anything
+    if(mealInput){
+
+    
+        const noMealFound = document.getElementById('no-meal-found');
+        noMealFound.innerText = ``;
+        const mealInfoSection = document.getElementById('meal-info-section');
+        mealInfoSection.innerHTML = ``;
+
+        // Clear meal details after every search
+        const mealDetailsSection = document.getElementById('meal-details-section');
+        mealDetailsSection.innerHTML = ``;
+
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s= ${mealInput}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            showMealInfoDiv(data, mealInput);
+        }
+        )
+    }
+    else{
+        const noMealFound = document.getElementById('no-meal-found');
+        noMealFound.innerText = `You haven't entered anything`;
+    }
+}
+
+document.getElementById('meal-submit').addEventListener('click',searchMeal);
+
+//Displaying meal details
+const showMealDetailsDiv = data => {
+    const meal = data.meals[0];
+    const mealPhoto = meal.strMealThumb;
+    const mealName = meal.strMeal;
+
+    const mealDetailsSection = document.getElementById('meal-details-section');
+    mealDetailsSection.innerHTML = `
+        <div id="meal-details" class="card px-0 pb-1 border-0 shadow col-xm-12 col-sm-12 col-md-6" style="border-radius: 10px;">
+            <img src="${mealPhoto}" class="card-img-top" style="border-radius: 10px 10px 0 0;" alt=" ...">
+            <div class="card-body">
+                <h2 class="card-title text-center my-3">${mealName}</h2>
+                <hr>
+                <h5 class="card-title mt-4">Meal Ingredients</h5>
+                <div id="meal-ingredients"></div>
+            </div>
+        </div>
+    `
+    const mealIngredients = document.getElementById('meal-ingredients');
+
+    // Set the contents of every paragraph in the meal details 
+    for(let i = 1; meal[`strIngredient${i}`]; i++){
+        const ingredients = `
+        ✔ ${meal[`strMeasure${i}`]} ${meal[`strIngredient${i}`]}
+        `
+        const mealDetailsP = document.createElement('p');
+        mealDetailsP.className = 'card-text';
+        mealDetailsP.innerText = ingredients;
+        mealIngredients.appendChild(mealDetailsP);
+    }
+}
+
+const getMealDetails = mealID => {
+    // Clear the meal details section for a new search
+    const mealDetailsSection = document.getElementById('meal-details-section');
+    mealDetailsSection.innerHTML = ``;
+
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showMealDetailsDiv(data));
+}
